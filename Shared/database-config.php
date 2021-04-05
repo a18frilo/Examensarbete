ea5c6559163d990e129f1421a1f75c967ef70824
Frida Lövborg <frida.lovborg@boid.se>
2021-04-05 19:33:41 +0200

    Database config file

---
+++ Shared/database-config.php
@@ -0,0 +1,57 @@
+<?php
+// Rename this file to database.php
+// Add the correct database information in pdoConnect()
+
+include_once("../../coursesyspw.php");
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+// dbconnect - Makes database connection
+//---------------------------------------------------------------------------------------------------------------
+$pdo = null;
+
+//---------------------------------------------------------------------------------------------------------------
+// err - Displays nicely formatted error and exits
+//---------------------------------------------------------------------------------------------------------------
+function err ($errmsg,$hdr='')
+{
+	if(!empty($hdr)){
+			echo($hdr);
+	}
+	print "<p><span class=\"err\">Serious Error: <br /><i>$errmsg</i>.";
+	print "</span></p>\n";
+	exit;
+}
+
+function dbConnect()
+{
+	$printHeaderFunction=0;
+	// Send header info to err()?
+	if ($printHeaderFunction) {
+		$hdr = 'Database Connection Error';
+	} else {
+		$hdr = '';
+	}
+
+	// Connect to DB server
+	$OC_db = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or err("Could not connect to database ".mysql_errno(),$hdr);
+	mysql_set_charset('utf8',$OC_db);
+	// Select DB
+	mysql_select_db(DB_NAME) or err("Could not select database \"".DB_NAME."\" error code".mysql_errno(),$hdr);
+}
+
+function pdoConnect()
+{
+	global $pdo;
+	try {
+		$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASSWORD);
+		if(!defined("MYSQL_VERSION")) {
+			define("MYSQL_VERSION",$pdo->query('select version()')->fetchColumn());
+		}
+	} catch (PDOException $e) {
+		echo "Failed to get DB handle: " . $e->getMessage() . "</br>";
+		exit;
+	}
+}
+?>
